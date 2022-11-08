72a9a6a22c9d69aebda9f480cc146b0633122fcf
Седельников Владимир Владимирович <vsedelnikov@fil-it.ru>
2022-11-08 13:56:32 +0300

    add 5.8 chapter

--- src/5 part/5.8/9th.py
+++ src/5 part/5.8/9th.py
@@ -1,11 +1,3 @@
 inp = input().split()
 
-lst_city = [x for i, x in enumerate(inp) if i % 2 == 0]
-lst_pop = [int(x) for i, x in enumerate(inp) if i % 2 == 1]
-lst = []
-
-for i in range(len(lst_city)):
-    pare = [lst_city[i], lst_pop[i]]
-    lst.append(pare)
-
-print(lst)
+print([[inp[i], int(inp[i + 1])] for i in range(0, len(inp), 2)])
